effd2787c2e026967980a195b2d8bc761186c43d
cyberbikepunk <loic@cyberpunk.bike>
2015-08-26 00:58:24 +0200

    Complete refactoring of the user module.
     This lead to adjacent changes to the main and settings modules. Also created the database module to take care of database load and filtering operations.

--- m5/user.py
+++ m5/user.py
@@ -1,171 +1,92 @@
-""" Connect the user to the local and remote databases. """
+""" This module connects the user locally (to the database) and remotely (to the company website). """
 
-from collections import namedtuple
+
 from getpass import getpass
-from glob import iglob
-from pandas import DataFrame, read_sql
 from requests import Session as RemoteSession
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from os.path import join, isdir, getctime
-from pandas import merge
-from os import mkdir, chmod, listdir
-from numpy import int64
+from os.path import isdir
+from os import mkdir
 
-from m5.settings import DEBUG, LOGIN_URL, LOGOUT_URL, DATABASE_DIR, STEP, USER_DIR, OUTPUT_DIR
-from m5.settings import TEMP_DIR, LOG_DIR, DOWNLOADS, OFFLINE, LEAP, SKIP
+from m5.settings import USER_DIR, LOGIN_URL, LOGOUT_URL, STEP, LOGGED_IN, REDIRECT, EXIT
 from m5.model import Model
-
-Database = namedtuple('Database', ['joined', 'orders', 'clients', 'checkins', 'checkouts'])
 
 
 class User:
-    """ Users work for Messenger (http://messenger.de). """
-
-    def __init__(self, username=None, password=None, db_file=None):
-        """ Authenticate the user and load data from the local database. """
+    def __init__(self, username=None, password=None,
+                 offline=False, verbose=False):
 
         self.username = username
         self.password = password
 
-        if not OFFLINE:
+        self.offline = offline
+        self.vernose = verbose
+
+        self.user_dir = USER_DIR
+        self.db_dir = USER_DIR + '/db'
+        self.log_dir = USER_DIR + '/log'
+        self.temp_dir = USER_DIR + '/temp'
+        self.output_dir = USER_DIR + '/output'
+        self.downloads_dir = USER_DIR + '/downloads'
+
+        self.sqlite_uri = 'sqlite:///' + self.db_dir + '/' + username + '.sqlite'
+        self.engine = None
+        self.model = None
+
+        self.local_session = None
+        self.remote_session = None
+
+    def initialize(self):
+        if not self.offline:
             self.remote_session = RemoteSession()
-            self._authenticate(username, password)
-        else:
-            self.remote_session = None
+            self._authenticate(self.username, self.password)
 
         self._check_install()
+        self._connect_to_db()
 
-        self.db_file = db_file if db_file else self._latest_db
-        path = join(DATABASE_DIR, self.db_file)
-        sqlite = 'sqlite:///{db}'.format(db=path)
-
-        self.engine = create_engine(sqlite, echo=DEBUG)
+    def _connect_to_db(self):
+        self.engine = create_engine(self.sqlite_uri, echo=DEBUG)
         self.model = Model.metadata.create_all(self.engine)
         self.local_session = sessionmaker(bind=self.engine)()
 
-        self.db = None
-        self._load_db()
-
-    @staticmethod
-    def _check_install():
-        """ Create user folders as needed. """
-
-        folders = (USER_DIR, OUTPUT_DIR, DATABASE_DIR, DOWNLOADS, TEMP_DIR, LOG_DIR)
+    def _check_install(self):
+        folders = (self.user_dir,
+                   self.output_dir,
+                   self.db_dir,
+                   self.downloads_dir,
+                   self.temp_dir,
+                   self.log_dir)
 
         for folder in folders:
             if not isdir(folder):
-                # Setting permissions directly with mkdir gives me bizarre results. Why?
                 mkdir(folder)
-                chmod(folder, 0o755)
-                print('Created %s' % folder)
 
-    @property
-    def _latest_db(self):
-        """ Return the most recent database. """
-
-        if listdir(DATABASE_DIR):
-            filepath = max(iglob(join(DATABASE_DIR, '*.sqlite')), key=getctime)
-        else:
-            filepath = '%s.sqlite' % self.username
-
-        print('Selected %s' % filepath)
-        return filepath
-
-    def _load_db(self) -> DataFrame:
-        """ Pull the database tables into Pandas and join them. """
-
-        eng = self.engine
-
-        clients = read_sql('client', eng, index_col='client_id')
-        orders = read_sql('order', eng, index_col='order_id', parse_dates=['date'])
-        checkins = read_sql('checkin', eng, index_col='timestamp', parse_dates=['timestamp', 'after_', 'until'])
-        checkpoints = read_sql('checkpoint', eng, index_col='checkpoint_id')
-        checkpoints = self._typecast_checkpoint_ids(checkpoints)
-
-        checkins_with_checkpoints = merge(left=checkins,
-                                          right=checkpoints,
-                                          left_on='checkpoint_id',
-                                          right_index=True,
-                                          how='left')
-
-        orders_with_clients = merge(left=orders,
-                                    right=clients,
-                                    left_on='client_id',
-                                    right_index=True,
-                                    how='left')
-
-        joined = merge(left=checkins_with_checkpoints,
-                       right=orders_with_clients,
-                       left_on='order_id',
-                       right_index=True,
-                       how='left')
-
-        joined.sort_index(inplace=True)
-        clients.sort_index(inplace=True)
-        orders.sort_index(inplace=True)
-        checkins.sort_index(inplace=True)
-        checkpoints.sort_index(inplace=True)
-
-        self.db = Database(joined, orders, clients, checkins, checkpoints)
-        print('Loaded the database into Pandas.', end=LEAP)
-
-        if DEBUG:
-            print('ORDERS:', end=SKIP)
-            print(orders.info(), end=LEAP)
-            print('CLIENTS:', end=SKIP)
-            print(clients.info(), end=LEAP)
-            print('CHECKINS:', end=SKIP)
-            print(checkins.info(), end=LEAP)
-            print('CHECKPOINTS:', end=SKIP)
-            print(checkpoints.info(), end=LEAP)
-            print('JOINED:', end=SKIP)
-            print(joined.info(), end=SKIP)
+        print('Created all user folders.')
 
     def _authenticate(self, username=None, password=None):
-        """ Make recursive login attempts. """
+        """ Make recursive login attempts to the website. """
 
-        self.username = username if username else input('Enter username: ')
-        self.password = password if password else getpass('Enter password: ')
+        if not username:
+            self.username = input('Enter username: ')
+        if not password:
+            self.password = getpass('Enter password: ')
 
-        # The server doesn't seem to care anyways.
-        headers = {'user-agent': 'Mozilla/5.0'}
-        self.remote_session.headers.update(headers)
+        response = self.remote_session.post(LOGIN_URL,
+                                            {'username': self.username,
+                                             'password': self.password})
 
-        url = LOGIN_URL
-        credentials = {'username': self.username, 'password': self.password}
-        response = self.remote_session.post(url, credentials)
-
-        # Login failure gives me a 200 response.
-        if 'erfolgreich' not in response.text:
-            print('Wrong credentials. Try again.', end=STEP)
+        if LOGGED_IN not in response.text:
+            print('Wrong credentials.', end=STEP)
             self._authenticate(None, None)
         else:
             print('Logged into the remote server.')
 
-    @staticmethod
-    def _typecast_checkpoint_ids(checkpoints):
-        # This bug has been fixed but we keep this for older versions of the database.
-        if checkpoints.index.dtype != 'int64':
-            checkpoints.reset_index(inplace=True)
-            checkpoint_ids = checkpoints['checkpoint_id'].astype(int64, raise_on_error=True)
-            checkpoints['checkpoint_id'] = checkpoint_ids
-            checkpoints.set_index('checkpoint_id', inplace=True)
-        return checkpoints
+    def logout(self):
+        if not self.offline:
+            response = self.remote_session.get(LOGOUT_URL, params=EXIT)
 
-    def quit(self):
-        """ Make a clean exit. """
-
-        if not OFFLINE:
-            url = LOGOUT_URL
-            payload = {'logout': '1'}
-            response = self.remote_session.get(url, params=payload)
-
-            if response.history[0].status_code == 302:
-                # We've been redirected to the login page:
-                # either we've successfully logged out or
-                # our request session had timed-out anyways.
-                print('Logged out. Goodbye!')
+            if response.history[0].status_code == REDIRECT:
+                print('Logged out from the remote server.')
             self.remote_session.close()
 
         exit(0)
@@ -173,4 +94,6 @@ class User:
 
 if __name__ == '__main__':
     user = User()
-    user.quit()
+
+    user.initialize()
+    user.logout()

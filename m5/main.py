cc7a36594b1c8dc6f9c896c4a27784538f3a5f23
cyberbikepunk <loic@cyberpunk.bike>
2015-08-27 17:17:10 +0200

    CLI enables arguments from file and validates dates.

--- m5/main.py
+++ m5/main.py
@@ -3,10 +3,10 @@
 
 
 from argparse import ArgumentParser, RawDescriptionHelpFormatter
-from logging import basicConfig, INFO, DEBUG, debug, info
+from logging import basicConfig, INFO, DEBUG, info
 from datetime import date
 from textwrap import dedent
-from timestring import Date
+from time import strptime
 
 from m5.scraper import scrape
 
@@ -24,32 +24,35 @@ def build_parser():
     p = ArgumentParser(prog='m5',
                        description=dedent(__doc__),
                        formatter_class=RawDescriptionHelpFormatter,
-                       fromfile_prefix_chars='@')
+                       fromfile_prefix_chars='@',
+                       epilog='To read arguments from file, type: m5 @/absolute/path/to/file.')
 
-    p.add_argument('-u', '--username',
+    p.add_argument('-u',
                    help='username for the company website',
                    type=str,
                    default=None,
                    dest='username')
 
-    p.add_argument('-p', '--password',
+    p.add_argument('-p',
                    help='password for the company website',
                    type=str,
                    default=None,
                    dest='password')
 
-    p.add_argument('-v', '--verbose',
+    p.add_argument('-v',
                    help='switch the verbose mode on',
                    default=False,
-                   action='store_true')
+                   action='store_true',
+                   dest='verbose')
 
-    p.add_argument('-o', '--offline',
+    p.add_argument('-o',
                    help='switch the offline mode on',
                    default=False,
-                   action='store_true')
+                   action='store_true',
+                   dest='offline')
 
-    p.add_argument('-s', '--since',
-                   help='scrape all data since that date (dd-mm-YYYY)',
+    p.add_argument('-s',
+                   help='scrape all data since dd-mm-yyyy',
                    type=since,
                    default=date.today(),
                    dest='since')
@@ -58,21 +61,18 @@ def build_parser():
 
 
 def since(date_string):
-    day = Date(date_string)
+    t = strptime(date_string, '%d-%m-%Y')
+    day = date(t.tm_year, month=t.tm_mon, day=t.tm_mday)
+
     if day > date.today():
-        raise ValueError('Cannot return to the future.')
-    return day.date
+        raise ValueError
 
-
-def get_options(namespace):
-    dictionary = {key: value for (key, value) in vars(namespace).items()}
-    info('Arguments = %s', dictionary)
-    return dictionary.pop('verbose')
+    return day
 
 
 if __name__ == '__main__':
     parser = build_parser()
     args = parser.parse_args()
     setup_logger(args.verbose)
-    options = get_options(args)
-    #scrape(**options)
+    info('Arguments = %s', args)
+    #scrape(**vars(args))

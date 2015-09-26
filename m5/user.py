cd206b9b76389b5c3f49842efc38f373e2f9f7e5
cyberbikepunk <loic@cyberpunk.bike>
2015-09-26 13:13:15 +0200

    Refactored the user authentication method.

--- m5/user.py
+++ m5/user.py
@@ -6,13 +6,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from os.path import isdir, join
 from os import mkdir
-from logging import debug, info
+from logging import info
 
 from m5.settings import OUTPUT_DIR, LOGIN_URL, LOGOUT_URL, LOGGED_IN, REDIRECT, EXIT, DATABASE_DIR
 from m5.model import Model
 
 
-class UserError(Exception):
+class UserException(Exception):
     pass
 
 
@@ -24,7 +24,12 @@ def initialize(**kwargs):
 
 
 class User:
-    def __init__(self, username=None, password=None, offline=False, verbose=False):
+    def __init__(self,
+                 username=None,
+                 password=None,
+                 offline=False,
+                 verbose=False):
+
         self.username = username
         self.password = password
 
@@ -43,11 +48,16 @@ class User:
 
     def authenticate(self):
         if self.offline:
-            self._local_authenticate()
+            self._check_directories()
+
         else:
-            self.web_session = Session()
-            self._remote_authenticate()
-            self._check_install()
+            response = self._login()
+            if LOGGED_IN in response.text:
+                self._make_directories()
+            else:
+                raise UserException('Wrong website credentials')
+
+            info('Remote authenticated')
 
     def start_db(self):
         self.sqlite_uri = 'sqlite://' + DATABASE_DIR + '/sqlite.db'
@@ -55,32 +65,31 @@ class User:
         self.model = Model.metadata.create_all(self.engine)
         self.db_session = sessionmaker(bind=self.engine)()
 
-        debug('Switched on %s', self.sqlite_uri)
+        info('Switched on database')
 
-    def _check_install(self):
+    def _check_directories(self):
+        if not isdir(self.downloads) or not isdir(self.output):
+            raise UserException('No existing directories')
+
+    def _make_directories(self):
         mkdir(self.downloads)
         mkdir(self.output)
-        debug('Created user folders')
 
-    def _local_authenticate(self):
-        return isdir(self.downloads) and isdir(self.output)
+        info('Created user folders')
 
     def _login(self):
-        credentials = {'username': self.username, 'password': self.password}
-        return self.web_session.post(LOGIN_URL, credentials)
+        self.web_session = Session()
 
-    def _remote_authenticate(self):
-        response = self._login()
-        if LOGGED_IN in response.text:
-            info('Remote authenticated')
-        else:
-            raise UserError('Bad credentials')
+        credentials = {'username': self.username,
+                       'password': self.password}
+
+        return self.web_session.post(LOGIN_URL, credentials)
 
     def quit(self):
         if not self.offline:
             response = self.web_session.get(LOGOUT_URL, params=EXIT)
+
             if response.history[0].status_code == REDIRECT:
                 self.web_session.close()
 
         info('Goodbye!')
-        exit(0)
